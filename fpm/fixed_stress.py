a6f2abd43206fc195d0e1b1706f621c61b6d4b58
Yury Zabegaev <yuri.zabegaev@uib.no>
2024-04-17 12:38:12 +0000

    checkpoint

--- fpm/fixed_stress.py
+++ fpm/fixed_stress.py
@@ -6,14 +6,18 @@ from pp_utils import get_fixed_stress_stabilization
 from block_matrix import BlockMatrixStorage
 
 
-def assemble_localization_matrices_2_15(bmat: BlockMatrixStorage) -> list:
+def assemble_localization_matrices_mechanics(
+    bmat: BlockMatrixStorage,
+    base: int,
+    nd: int,
+) -> list:
     # 2: A  Q1Q1Q1
     # 1: Q2 B   M1
     # 5: Q2 M2  P
-    bmat = bmat[[2, 1, 5]]
+    bmat = bmat[[base, 1, 5]]
 
-    Q1 = bmat[2, [1, 5]].mat.tocsr()
-    Q2 = bmat[[1, 5], 2].mat.tocsc()
+    Q1 = bmat[base, [1, 5]].mat.tocsr()
+    Q2 = bmat[[1, 5], base].mat.tocsc()
     M1 = bmat[1, 5].mat.tocsc()
     M2 = bmat[5, 1].mat.tocsr()
 
@@ -21,9 +25,13 @@ def assemble_localization_matrices_2_15(bmat: BlockMatrixStorage) -> list:
 
     restrictions = []
 
-    for frac_dof in range(Q1.shape[0]):
-        restr_q1 = Q1[frac_dof, :].indices
-        restr_q2 = Q2[:, frac_dof].indices
+    assert (Q1.shape[0] % nd) == 0
+    num_cells = Q1.shape[0] // nd
+
+    for i in range(num_cells):
+        frac_dofs = np.arange(nd * i, nd * (i + 1))
+        restr_q1 = Q1[frac_dofs, :].indices
+        restr_q2 = Q2[:, frac_dofs].indices
         restr = np.unique(np.concatenate([restr_q1, restr_q2]))
 
         restr_local = restr - B_size
@@ -48,15 +56,15 @@ def assemble_localization_matrices_2_15(bmat: BlockMatrixStorage) -> list:
     return restrictions
 
 
-def make_local_prec_2_15(bmat: BlockMatrixStorage):
-    localization_mats = assemble_localization_matrices_2_15(bmat)
+def make_local_inverse_15(bmat: BlockMatrixStorage, base, nd):
+    localization_mats = assemble_localization_matrices_mechanics(bmat, base=base, nd=nd)
 
     J_15 = bmat[[1, 5]].mat
     J15_inv = csr_zeros(J_15.shape[0])
 
     for R in localization_mats:
-        # j15 = R @ J_15 @ R.T
-        j15 = R @ R.T
+        j15 = R @ J_15 @ R.T
+        # j15 = R @ R.T
 
         j15_inv = scipy.sparse.linalg.inv(j15)
         J15_inv += R.T @ j15_inv @ R
@@ -64,9 +72,13 @@ def make_local_prec_2_15(bmat: BlockMatrixStorage):
     return J15_inv
 
 
+def make_local_stab_15(bmat: BlockMatrixStorage, base: int, nd: int):
+    J15_inv = make_local_inverse_15(bmat=bmat, base=base, nd=nd)
+    return -bmat[base, [1, 5]].mat @ J15_inv @ bmat[[1, 5], base].mat
+
+
 def make_fs(model, J: BlockMatrixStorage):
     result = J.empty_container()[[0, 2]]
     result[0, 0] = get_fixed_stress_stabilization(model)
-    J_15_inv = make_local_prec_2_15(J)
-    result[2, 2] = -J[2, [1, 5]].mat @ J_15_inv @ J[[1, 5], 2].mat
+    result[2, 2] = make_local_stab_15(bmat=J, base=2, nd=1)
     return result
